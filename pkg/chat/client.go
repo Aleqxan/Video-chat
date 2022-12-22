b3fce58c7fb69abdb7ec0c059dcf7400afbdca79
Aleqxan <inno.alex@ymail.com>
2022-12-22 17:29:01 +0100

    Worked on client.go file

--- pkg/chat/client.go
+++ pkg/chat/client.go
@@ -2,6 +2,9 @@ package chat
 
 import (
 	"time"
+	"bytes"
+	"log"
+	
 	"github.com/fasthttp/websocket"
 )
 
@@ -12,20 +15,52 @@ const(
 	maxMessageSize 	= 512
 )
 
+var (
+	newline = []byte{'\n'}
+	space = []byte{' '}
+)
+
 type Client struct {
 	Hub *Hub 
 	Conn *websocket.Conn
 	Send chan []byte
 }
 
-func (c *Client) readPump(){
+var upgrader = websocket.FastHTTPUpgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+}
 
+func (c *Client) readPump(){
+	defer func(){
+		c.Hub.unregister <- c 
+		c.Conn.Close()
+	}()
+
+	c.Conn.SetReadLimit (maxMessageSize)
+	c.Conn.setReadDeadline(time.Now().Add(pongWait))
+	c.Conn.SetPongHandler(func(string) error {c.Conn.setReadDeadline(time.Now().Add(pongWait)); return nil})
+	for {
+		_, message, err := c.Conn.ReadMessage()
+		if  err != nil {
+			if websocket.IsUnexpectedClosedError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure){
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		message = bytes.TrimSpace(bytes.Replace(messages, newline, space, -1))
+		c.Hub.broadcast <- message
+	}
 }
 
 func (c *Client) writePump(){
 
 }
 
-func PeerChatConn(){
-	
+func PeerChatConn(c *websocket.Conn, hub *Hub){
+	client := &Client{Hub: hub, Conn: c, Send: make(chan []byte, 256)}
+	clinet.Hu .register <- client
+
+	go client.writePump()
+	client.readPump()
 }
